ac39a2f3155ede27937d58163662c2c1145c6839
Selinawf <selinawangf@gmail.com>
2026-02-26 00:38:51 -0500

    added rewards banner

--- scripts/rewards.js
+++ scripts/rewards.js
@@ -1,26 +1,50 @@
 import gameState from "./gameState.js";
 
+let goal = 25;
+
+/**
+ * Reveals a banner when the user reaches a certain goal
+ */
 export function initRewards() {
+  if (document.getElementById("rewards-banner")) return;
+
+  //creates a banner
   const banner = document.createElement("div");
   banner.id = "rewards-banner";
   banner.classList.add("hidden");
-  document.body.insertBefore(banner, document.body.firstChild);
+  document.body.prepend(banner);
 
   let bannerTimer = null;
 
-  function showBanner(msg, ms = 2500) {
-    banner.textContent = msg;
-    banner.classList.remove("hidden");
+  /**
+   * Reveals a goal banner for a short period of time
+   */
+  function showBanner(message, ms = 2500) {
+    banner.textContent = message;
+    clearTimeout(bannerTimer);
+    banner.classList.add("hidden");
 
-    if (bannerTimer) clearTimeout(bannerTimer);
-    bannerTimer = setTimeout(() => banner.classList.add("hidden"), ms);
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        banner.classList.remove("hidden"); // fade in
+        bannerTimer = setTimeout(() => {
+          banner.classList.add("hidden"); // fade out
+        }, ms);
+      });
+    });
   }
 
-  const goals = new Set([10, 25, 50, 100]);
 
+  /**
+   * Listener that tracks when user meets the certain amount of clicks
+   */
   gameState.addClickListener((clicks) => {
-    if (goals.has(clicks)) {
-      showBanner(`${clicks} clicks!`);
+    if (clicks >= goal) {
+      showBanner(`Reached ${goal} clicks!`);
+      console.log(`reach ${goal}`);
+
+      goal *= 2;
+      console.log(`next goal: ${goal}`)
     }
   });
 }
